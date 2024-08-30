1be26c002bc4c92fc4f48caaeed79a2defdc7283
MinecraftChromeDev <therealsamdan90@gmail.com>
2024-08-30 23:50:13 +0000

    Update

--- Lexicon-Lexxie/Who_am_I_files/Who_am_I.js
+++ Lexicon-Lexxie/Who_am_I_files/Who_am_I.js
@@ -1,9 +1,22 @@
 // Created by iWeb 3.0.2 local-build-20240830
 
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Who_am_I_files/Who_am_IMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+setTransparentGifURL("Media/transparent.gif");
+function applyEffects() {
+  var registry = IWCreateEffectRegistry();
+  registry.registerEffects({ stroke_0: new IWEmptyStroke() });
+  registry.applyEffects();
+}
+function hostedOnDM() {
+  return false;
+}
+function onPageLoad() {
+  loadMozillaCSS("Who_am_I_files/Who_am_IMoz.css");
+  adjustLineHeightIfTooBig("id1");
+  adjustFontSizeIfTooBig("id1");
+  adjustLineHeightIfTooBig("id2");
+  adjustFontSizeIfTooBig("id2");
+  adjustLineHeightIfTooBig("id3");
+  adjustFontSizeIfTooBig("id3");
+  fixAllIEPNGs("Media/transparent.gif");
+  applyEffects();
+}
